1dc4ec5869a76d502d2eeeeec1f92d7f9a21b309
Eloiza Raymundo <72866068+Izaaaajy@users.noreply.github.com>
2021-10-14 16:31:59 +0800

    Add files via upload

---
+++ resources/views/pages/donor.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.sidebar')
+
+@extends('layouts.navbar')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Boxicons -->
+<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+<!-- My CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>Donor</title>
+</head>
+<body>
+
+
+<!-- CONTENT -->
+<section id="content">
+
+<!-- MAIN -->
+            <main>
+              <div class="head-title">
+                <div class="left">
+                    <h1>Donor</h1>
+                        <ul class="breadcrumb">
+                        
+              <li>
+
+                  <a href="/donor">Donor</a>
+              </li>
+                    <li><i class='bx bx-chevron-right' ></i></li>
+              <li>
+                  <a class="active" href="#">Home</a>
+              </li>
+              </ul>
+
+            </main>
+<!-- MAIN -->
+</section>
+<!-- CONTENT -->
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
